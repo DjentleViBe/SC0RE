51c5d3c6dd41ba6cca05eaaa04e632a6b8448597
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2024-08-23 03:57:34 +0200

    main function

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ import guitarpro
 from preprocess import readgpro, guitarinfo
 from encoding import getencoding
 
-gprofolder = './gprofiles/'
-song = readgpro(gprofolder + 'Example-1.gp5')
-tuning = guitarinfo(song)
-
-print("Note String Duration")
-for track in song.tracks:
-    # Map values to Genaral MIDI.
-    for measure in track.measures:
-        for voice in measure.voices:
-            for beat in voice.beats:
-                for note in beat.notes:
-                    # note.value = encoding.MAPPING_NOTE.get(note.value, note.value)
-                    # print(note.value, note.string, note.beat.duration.value)
-                    print(getencoding(note.value, note.string, tuning), note.beat.duration.value)
+if __name__ == '__main__':
+    gprofolder = './gprofiles/'
+    song = readgpro(gprofolder + 'Example-1.gp5')
+    tuning = guitarinfo(song)
+    print("Note Duration")
+    
+    for track in song.tracks:
+        # Map values to Genaral MIDI.
+        for measure in track.measures:
+            for voice in measure.voices:
+                for beat in voice.beats:
+                    for note in beat.notes:
+                        # note.value = encoding.MAPPING_NOTE.get(note.value, note.value)
+                        # print(note.value, note.string, note.beat.duration.value)
+                        print(getencoding(note.value, note.string, tuning), note.beat.duration.value)
