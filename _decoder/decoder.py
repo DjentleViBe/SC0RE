b1e45ce8405fc59895957b45f520c7bc8bc1e561
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2024-08-28 10:55:24 +0200

    decoder working version

---
+++ _decoder/decoder.py
@@ -0,0 +1,63 @@
+"""decoder part of the transformer
+"""
+# import torch
+from torch import nn
+from _encoder.encoder import LayerNormalization, PositionwiseFeedForward,\
+                    MultiHeadAttentionAPE
+
+class DecoderLayerAPE(nn.Module):
+    """Decoder layer
+    """
+    def __init__(self, device, d_model, d_vocab, seq_length, ffn_hidden, num_heads, drop_prob):
+        super().__init__()
+        self.self_attention = MultiHeadAttentionAPE(device, d_model, seq_length, num_heads)
+        self.layer_norm1 = LayerNormalization(device, parameters_shape=[d_model])
+        self.dropout1 = nn.Dropout(p=drop_prob)
+
+        self.ffn = PositionwiseFeedForward(device, d_model, ffn_hidden, drop_prob)
+        self.layer_norm3 = LayerNormalization(device, parameters_shape=[d_model])
+        self.dropout3 = nn.Dropout(p=drop_prob)
+        self.linear_layer = nn.Linear(d_model, d_model)
+        self.vocab_layer = nn.Linear(d_model, d_vocab)
+
+    def forward(self, x_val, y_val, decoder_mask):
+        """Forward prop
+        """
+        _y = y_val.clone()
+        y_val = self.self_attention(y_val, mask=decoder_mask)
+        y_val = self.dropout1(y_val)
+        y_val = self.layer_norm1(y_val + _y)
+
+        # _x = x_val.clone()
+        _y = y_val.clone()
+        y_val = self.ffn(y_val)
+        y_val = self.dropout3(y_val)
+        y_val = self.layer_norm3(y_val + _y)
+        y_val = self.linear_layer(y_val)
+        y_val = self.vocab_layer(y_val)
+        return y_val
+
+class SequentialDecoder(nn.Sequential):
+    """To pass more than one parameter in sequential
+    """
+    def forward(self, *inputs):
+        """forward pass for custom seq decoder
+        """
+        xseq, yseq, mask = inputs
+        for module in self._modules.values():
+            yseq = module(xseq, yseq, mask)
+        return yseq
+
+class DecoderAPE(nn.Module):
+    """Main decoder class
+    """
+    def __init__(self, device, d_model, d_vocab, ffn_hidden, seq_length, num_heads, drop_prob, num_layers) -> None:
+        super().__init__()
+        self.layers = SequentialDecoder(*[DecoderLayerAPE(device, d_model, d_vocab, seq_length,ffn_hidden,num_heads,drop_prob)\
+                                      for _ in range(num_layers)])
+
+    def forward(self, x_val, y_val, decoder_mask):
+        """Forward layer
+        """
+        x_val = self.layers(x_val, y_val, decoder_mask)
+        return x_val
--- main.py
+++ main.py
@@ -4,13 +4,31 @@ import numpy as np
 # import guitarpro
 import torch
 from torch import nn
+import torch.nn.functional as F
 from preprocess import readgpro, guitarinfo, get_positional_encoding
 from encoding import getencodingnotes, getencodingbeats
-from _encoder.encoder import EncoderAPE
+# from _encoder.encoder import EncoderAPE
+from _decoder.decoder import DecoderAPE
 
 if __name__ == '__main__':
-    training_src_notes = np.zeros((5, 10), dtype = 'int32')
-    training_data_beats = np.zeros((5, 10), dtype = 'int32')
+
+    ################################ transformers #################################
+    ########## Params ##############
+    EPOCHS          =   100
+    D_MODEL         =   512
+    VOCAB_SIZE      =   132
+    FFN_HIDDEN      =   1024
+    MAX_SEQ_LENGTH  =   5
+    NUM_HEADS       =   4
+    DROP_PROB       =   0.1
+    NUM_LAYERS      =   1
+    LEARNING_RATE   =   0.1
+    PATCH           =   1
+    STRIDE          =   1
+    ################################
+
+    training_src_notes = np.zeros((5, MAX_SEQ_LENGTH), dtype = 'int32')
+    training_data_beats = np.zeros((5, MAX_SEQ_LENGTH), dtype = 'int32')
     GPROFOLDER = './gprofiles/'
     j = 0
     k = 0
@@ -42,9 +60,12 @@ if __name__ == '__main__':
 
     # training_src_notes = training_src_notes.reshape(5, 10, 1)
     training_tgt_notes = training_src_notes.copy()
-    print("Notes")
+    # print("Notes")
     training_src_notes[:,4] = 0
+    print("Source")
     print(training_src_notes)
+    print("Target")
+    print(training_tgt_notes)
     #print("Beats")
     #print(training_data_beats)
 
@@ -55,48 +76,70 @@ if __name__ == '__main__':
     #print("target")
     #print(training_tgt_notes)
 
-
-    ################################ transformers #################################
-    ########## Params ##############
-    D_MODEL         =   512
-    VOCAL_SIZE      =   132
-    FFN_HIDDEN      =   1024
-    MAX_SEQ_LENGTH  =   10
-    NUM_HEADS       =   4
-    DROP_PROB       =   0.1
-    NUM_LAYERS      =   1
-    LEARNING_RATE   =   0.1
-    PATCH           =   2
-    STRIDE          =   2
-    ################################
+    iteration = 0
     device = torch.device("mps")
     criterion = torch.nn.MSELoss()
-    encoder = EncoderAPE(device, D_MODEL, FFN_HIDDEN, MAX_SEQ_LENGTH, NUM_HEADS,
-                         DROP_PROB, NUM_LAYERS)
-    optimizer = torch.optim.AdamW(encoder.parameters(), lr = LEARNING_RATE)
+    # encoder = EncoderAPE(device, D_MODEL, FFN_HIDDEN, MAX_SEQ_LENGTH, NUM_HEADS,
+    #                     DROP_PROB, NUM_LAYERS)
+    decoder = DecoderAPE(device, D_MODEL, VOCAB_SIZE, FFN_HIDDEN, MAX_SEQ_LENGTH, NUM_HEADS,
+                         DROP_PROB, NUM_LAYERS).to(device)
+    optimizer = torch.optim.AdamW(decoder.parameters(), lr = LEARNING_RATE)
 
-    encoder.train()
+    # encoder.train()
 
     NUM_PATCH = ((MAX_SEQ_LENGTH - PATCH)//STRIDE) + 1
     mask = torch.full([NUM_PATCH, NUM_PATCH], float('-inf'))
     mask = torch.triu(mask, diagonal = 1).to(device)
+    loss_fn = nn.CrossEntropyLoss()
 
-    with torch.set_grad_enabled(True):
-        embedding_layer = nn.Embedding(num_embeddings = VOCAL_SIZE,
-                                       embedding_dim = D_MODEL).to(device)
-        token_ids = torch.tensor(training_src_notes).to(device)
+    embedding_layer = nn.Embedding(num_embeddings = VOCAB_SIZE,
+                                    embedding_dim = D_MODEL).to(device)
+    token_ids = torch.tensor(training_src_notes).to(device)
 
+
+    pos_enc = get_positional_encoding(MAX_SEQ_LENGTH, D_MODEL).to(device)
+    # src = torch.from_numpy(training_src_notes).to(device)
+    target = torch.from_numpy(training_tgt_notes).to(device)
+    # prediction_val = encoder(input_embeddings).to(device)
+
+    while iteration <= EPOCHS:
+        decoder.train()
+
+        optimizer.zero_grad()
         embeddings = embedding_layer(token_ids)
-        pos_enc = get_positional_encoding(10, D_MODEL).to(device)
-
         input_embeddings = embeddings + pos_enc
 
-        # src = torch.from_numpy(training_src_notes).to(device)
-        # target = torch.from_numpy(training_tgt_notes).to(device)
+        logits = decoder(target, input_embeddings, mask)
+        # probabilities = F.softmax(prediction_val, dim=-1)
 
-        prediction_val = encoder(input_embeddings).to(device)
-        loss = criterion(target, prediction_val)
+        logits = logits.view(-1, VOCAB_SIZE)  # Flatten logits to (batch_size * seq_length, d_vocab)
+        target = target.view(-1)
+
+        loss = loss_fn(logits, target)
+        # loss = criterion(target, prediction_val)
+
+        print(iteration + 1, loss.item())
         loss.backward()
-
         optimizer.step()
-        optimizer.zero_grad()
+
+        iteration += 1
+
+        if loss.item() < 0.001:
+            break
+
+    decoder.eval()
+    dummy_in = torch.tensor(np.array([[89, 0, 0, 0, 0]], dtype = 'int32')).to(device)
+    embeddings = embedding_layer(dummy_in)
+    pos_enc_eval = get_positional_encoding(MAX_SEQ_LENGTH, D_MODEL).to(device)
+    output_eval = decoder(target, embeddings + pos_enc_eval, mask)
+
+    next_token_logits = output_eval[:, -1, :]
+    probabilities = F.softmax(next_token_logits, dim=-1)
+
+    # Select the next token (using greedy search here)
+    next_token = torch.argmax(probabilities, dim=-1).unsqueeze(0)
+
+    generated_sequence = torch.cat([dummy_in, next_token], dim=1)
+    print(generated_sequence)
+    print("Finished")
+
