d689729a68995a5d88bcb3b155d1ac3c3563b40d
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2024-09-05 10:43:03 +0200

    measure

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 
 ################################ transformers #################################
 MODE            =   1  # 0: train, # 1 : eval, # 2 : both
-BACKUP          =   "dec_only_notes_5"
+BACKUP          =   "dec_only_notes_6"
 START_ID        =   9592
 ########## Params ##############
 EPOCHS          =   1000
 VOCAB_SIZE      =   26412
 FFN_HIDDEN      =   1024
-MAX_SEQ_LENGTH  =   25
+MAX_SEQ_LENGTH  =   12
 NUM_HEADS       =   2
 DROP_PROB       =   0.3
 NUM_LAYERS      =   2
@@ -17,9 +17,10 @@ LEARNING_RATE   =   0.01
 PATCH           =   1
 STRIDE          =   1
 TRAINING        =   ["CB"]
-BATCH           =   424
+BATCH           =   890
 
 EOS             =   26405
 BOS             =   26406
 BARRE_NOTE      =   26407
 #################################################################################
+MEASURE         =   16
